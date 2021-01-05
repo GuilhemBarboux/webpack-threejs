9cced3cf7b08d335982c785d93cc544ce2ad581d
GuilhemBarboux <guilhembarboux@gmail.com>
2021-01-05 19:29:58 +0100

    Chore(Production): add plugin to bundle analyse and optimise

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,18 @@
 const { merge } = require("webpack-merge")
 const common = require("./common.js")
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static",
+    })
+  ],
+  optimization: {
+    usedExports: true,
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
 })
